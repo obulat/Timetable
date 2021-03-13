96bcaaf48a6c4780d9090cc6471a048150915a85
Olga Bulat <obulat@gmail.com>
2021-03-13 01:36:41 -0800

    Add periods and init data

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -54,7 +54,7 @@ var config = {
 
 		colors: {}
 
-		/*timetable: [
+		timetable: [
 			{
 				day: "Monday",
 				schedule: [
@@ -65,9 +65,7 @@ var config = {
 					{subject: "Biology", room: 113},
 					{subject: "Computer Science", room:312}, 
 					{subject: "", room: ""},
-					{subject: "Maths", room:329},
-					{subject: "Music", room:211},
-					{subject: "", room: ""}
+
 				]
 			},
 
@@ -81,9 +79,7 @@ var config = {
 					{subject: "English", room: 222},
 					{subject: "English", room: 222},
 					{subject: "Music",  room: 211},
-					{subject: "", room: ""},
-					{subject: "", room: ""},
-					{subject: "", room: ""}
+
 				]
 			},
 
@@ -96,10 +92,7 @@ var config = {
 					{subject: "Maths", room:111},
 					{subject: "Biology", room:113},
 					{subject: "Physics", room:321},
-					{subject: "", room: ""},
-					{subject: "", room: ""},
-					{subject: "", room: ""},
-					{subject: "", room: ""}
+
 				]
 			},
 
@@ -112,10 +105,7 @@ var config = {
 					{subject: "Computer Science", room:312},
 					{subject: "English", room:312},
 					{subject: "", room:""},
-					{subject: "Music", room:214},
-					{subject: "Religion", room:315},
-					{subject: "", room: ""},
-					{subject: "", room: ""}
+
 				]
 			},
 
@@ -128,10 +118,7 @@ var config = {
 					{subject: "Biology", room:113},
 					{subject: "Computer Science", room:216},
 					{subject: "English", room:217},
-					{subject: "Sport", room:"SHG"},
-					{subject: "Sport", room:"SHG"},
-					{subject: "", room: ""},
-					{subject: "", room: ""}
+
 				]
 			}
 		],
@@ -139,32 +126,25 @@ var config = {
 
 
 		periods: [
-			{start: "07:50", end: "08:35"},
-			{start: "08:40", end: "09:25"},
-			{start: "09:40", end: "10:25"},
-			{start: "10:30", end: "11:15"},
-			{start: "11:30", end: "12:15"},
-			{start: "12:15", end: "13:00"},
-			{start: "13:15", end: "14:00"},
-			{start: "14:00", end: "14:45"},
-			{start: "14:45", end: "15:30"},
-			{start: "15:30", end: "16:15"}
+			{start: "09:10", end: "09:40"},
+			{start: "09:50", end: "10:20"},
+			{start: "10:30", end: "11:00"},
+			{start: "11:10", end: "11:40"},
+			{start: "12:30", end: "13:00"},
+			{start: "13:10", end: "13:40"},
+			{start: "13:50", end: "14:20"},
+			{start: "15:50", end: "16:20"}
 		],
 
 
 
 		colors: {
-			"Physics":"179",
-			"English":"38",
-			"Biology":"82",
-			"German":"208",
-			"Maths":"134",
-			"Music":"0",
-			"History":"21",
-			"Religion":"282",
-			"Geography":"56",
-			"Computer Science":"326",
-			"Sport":"251"
-		}*/
+			"Matematik":"179",
+			"Fen":"38",
+			"Türkçe":"82",
+			"Din Kültürü":"208",
+			"Beden Eğitimi":"134",
+			"Görsel Sanatlar":"0"
+		}
 	}
 };
